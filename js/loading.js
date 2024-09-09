20a494ac4df77b837678d0ba2b7c7323fa907446
jeff <junior.fidelis.3386@gmail.com>
2024-09-09 12:25:13 -0300

    🔧 chore: ajustes finais do projeto e atualização do título da página
    🔄 Realizados ajustes finais no projeto.
    
    Mudanças incluem:
    - Correção dos caminhos das pastas para melhor organização
    - Atualização do título da página 'user-data.html' para 'Usuário cadastrado com sucesso'
    
    Essas alterações visam melhorar a estrutura do projeto e a experiência do usuário.

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,6 @@
 function outraPagina() {
-    if (window.location.pathname !== "/Desafios/Desafio-01-semana-04/pages/onboarding.html") {
-        window.location.href = "/Desafios/Desafio-01-semana-04/pages/onboarding.html";
+    if (window.location.pathname !== "/pages/onboarding.html") {
+        window.location.href = "/pages/onboarding.html";
     }
 }
 
